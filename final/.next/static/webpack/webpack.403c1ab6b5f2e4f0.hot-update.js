b3e14201aa6cbe0dd50682f3e41277e2af366b5c
SpreeGore <89117156+SpreeGore@users.noreply.github.com>
2024-02-15 20:25:12 -0700

    v1.3

--- final/.next/static/webpack/webpack.403c1ab6b5f2e4f0.hot-update.js
+++ final/.next/static/webpack/webpack.403c1ab6b5f2e4f0.hot-update.js
@@ -11,21 +11,8 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "403c1ab6b5f2e4f0"; }
+/******/ 	__webpack_require__.h = function() { return "ca32ca4678d5978a"; }
 /******/ }();
-/******/ 
-/******/ /* webpack/runtime/compat */
-/******/ 
-/******/ 
-/******/ // noop fns to prevent runtime errors during initialization
-/******/ if (typeof self !== "undefined") {
-/******/ 	self.$RefreshReg$ = function () {};
-/******/ 	self.$RefreshSig$ = function () {
-/******/ 		return function (type) {
-/******/ 			return type;
-/******/ 		};
-/******/ 	};
-/******/ }
 /******/ 
 /******/ }
 );
